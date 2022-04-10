30ffe17a7edd4f91478e92549ba1639b9ea7cdb5
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-04-10 19:23:46 +0300

    Add clear and backspace functionalities. Fix bug with calculation of decimal numbers. Refactor and optimize code.

--- app/src/main/java/com/dst/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/dst/basiccalculator/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.dst.basiccalculator
 
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.dst.basiccalculator.databinding.ActivityMainBinding
 import java.lang.StringBuilder
-import java.text.NumberFormat
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var binding: ActivityMainBinding
-    private var operator : String = ""
-    private var firstNumber : Double = 0.0
-    private var secondNumber : Double = 0.0
-    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    private var operator: String = ""
+    private var firstNumber: Double = 0.0
+    private var secondNumber: Double = 0.0
+    private var reset = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -39,101 +39,91 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonDivide.setOnClickListener(this)
         binding.buttonDot.setOnClickListener(this)
         binding.buttonEquals.setOnClickListener(this)
+        binding.buttonClear.setOnClickListener(this)
+        binding.buttonBackspace.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
-        val result = StringBuilder(binding.result.text)
         when (view.id) {
-            R.id.button_one -> {
-                result.append("1")
-                binding.result.text = result.toString()
-            }
-            R.id.button_two -> {
-                result.append("2")
-                binding.result.text = result.toString()
-            }
-            R.id.button_three -> {
-                result.append("3")
-                binding.result.text = result.toString()
-            }
-            R.id.button_four -> {
-                result.append("4")
-                binding.result.text = result.toString()
-            }
-            R.id.button_five -> {
-                result.append("5")
-                binding.result.text = result.toString()
-            }
-            R.id.button_six -> {
-                result.append("6")
-                binding.result.text = result.toString()
-            }
-            R.id.button_seven -> {
-                result.append("7")
-                binding.result.text = result.toString()
-            }
-            R.id.button_eight -> {
-                result.append("8")
-                binding.result.text = result.toString()
-            }
-            R.id.button_nine -> {
-                result.append("9")
-                binding.result.text = result.toString()
-            }
-            R.id.button_zero -> {
-                result.append("0")
-                binding.result.text = result.toString()
-            }
-            R.id.button_add -> {
-                operator = "+"
-                firstNumber = binding.result.text.toString().toDouble()
-                binding.result.text = ""
-            }
-            R.id.button_subtract -> {
-                operator = "-"
-                firstNumber = binding.result.text.toString().toDouble()
-                binding.result.text = ""
-            }
-            R.id.button_multiply -> {
-                operator = "*"
-                firstNumber = binding.result.text.toString().toDouble()
-                binding.result.text = ""
-            }
-            R.id.button_divide -> {
-                operator = "/"
-                firstNumber = binding.result.text.toString().toDouble()
-                binding.result.text = ""
-            }
-            R.id.button_dot -> {
-                if (!binding.result.text.contains(".")) {
-                    result.append(".")
-                    binding.result.text = result.toString()
-                }
-            }
-            R.id.button_equals -> {
-                secondNumber = binding.result.text.toString().toDouble()
-                calculateResult()
-            }
+            R.id.button_one -> addDigit(1)
+            R.id.button_two -> addDigit(2)
+            R.id.button_three -> addDigit(3)
+            R.id.button_four -> addDigit(4)
+            R.id.button_five -> addDigit(5)
+            R.id.button_six -> addDigit(6)
+            R.id.button_seven -> addDigit(7)
+            R.id.button_eight -> addDigit(8)
+            R.id.button_nine -> addDigit(9)
+            R.id.button_zero -> addDigit(0)
+            R.id.button_add -> setOperation("+")
+            R.id.button_subtract -> setOperation("-")
+            R.id.button_multiply -> setOperation("*")
+            R.id.button_divide -> setOperation("/")
+            R.id.button_dot -> addDotSymbol()
+            R.id.button_equals -> calculateResult()
+            R.id.button_clear -> clear()
+            R.id.button_backspace -> backspace()
         }
     }
 
     private fun calculateResult() {
-        when(operator) {
-            "*" -> {
-                val result = NumberFormat.getCurrencyInstance().format(firstNumber * secondNumber)
-                binding.result.text = getString(R.string.result, NumberFormat.getInstance().format(firstNumber * secondNumber))
-            }
-            "/" -> {
-                binding.result.text = getString(R.string.result, NumberFormat.getInstance().format(firstNumber / secondNumber))
-            }
-            "+" -> {
-                binding.result.text = getString(R.string.result, NumberFormat.getInstance().format(firstNumber + secondNumber))
-            }
-            "-" -> {
-                binding.result.text = getString(R.string.result, NumberFormat.getInstance().format(firstNumber - secondNumber))
-            }
+        var result = 0.0
+        secondNumber = binding.result.text.toString().toDouble()
+        binding.operator.text = ""
+        when (operator) {
+            "*" -> result = firstNumber * secondNumber
+            "/" -> result = firstNumber / secondNumber
+            "+" -> result = firstNumber + secondNumber
+            "-" -> result = firstNumber - secondNumber
+        }
+        val df = DecimalFormat("#.##########")
+        binding.result.text = getString(R.string.result, df.format(result))
+    }
+
+    //Reset first number when operation button is clicked
+    private fun reset(initialDigit: String) {
+        reset = false
+        binding.result.text = ""
+        binding.result.text = initialDigit
+    }
+
+    private fun clear() {
+        firstNumber = 0.00
+        secondNumber = 0.00
+        binding.operator.text = ""
+        binding.result.text = "0"
+    }
+
+    private fun addDigit(digit: Int) {
+        val result = StringBuilder(binding.result.text)
+        result.append(digit.toString())
+        if (reset) {
+            reset(digit.toString())
+        } else {
+            binding.result.text = result.toString()
         }
     }
 
+    private fun addDotSymbol() {
+        val result = StringBuilder(binding.result.text)
+        if (!binding.result.text.contains(".")) {
+            result.append(".")
+            binding.result.text = result.toString()
+        }
+    }
 
+    private fun setOperation(newOperator : String) {
+        binding.operator.text = newOperator
+        operator = newOperator
+        firstNumber = binding.result.text.toString().toDouble()
+        reset = true
+    }
+
+    private fun backspace() {
+        if (binding.result.text.isNotEmpty()) {
+            val number = StringBuilder(binding.result.text)
+            number.deleteCharAt(number.length - 1)
+            binding.result.text = number
+        }
+    }
 }
